37963efb7dd67c79059c76fcdb26efffed6c9df4
Aramidei <mideesther.re@gmail.com>
2022-09-08 15:57:31 -0700

    Print a text

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Printing a text
+ *
+ * Return: Always 0 (Sucessful)
+ */
+int main (void)
+{
+     printf("with proper grammar, but the outcome is a piece of art,\n");
+     
+     return (0)
+}     
