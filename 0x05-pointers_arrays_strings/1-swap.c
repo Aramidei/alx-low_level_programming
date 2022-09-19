ba9db71a226eb3ce9d916d1e1fb6ed22df436432
Aramidei <mideesther.re@gmail.com>
2022-09-19 06:48:35 -0700

    Reset

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+
+/**
+ *
+ * reset_to_98  - It reserts the value of a given integer to 98
+ * n: parameter
+ * Returns : nothing
+ */
+
+void reset_to_98(int *n)
+
+{
+	*n = 98;
+}
